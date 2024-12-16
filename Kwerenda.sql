d82a8a4d9ad28f17e48a57113d22ce5800eec888
Gabriela Król <gm.krol@student.uw.edu.pl>
2024-12-16 23:25:05 +0100

    Update Kwerenda.sql

--- Kwerenda.sql
+++ Kwerenda.sql
@@ -1,11 +1,6 @@
 CREATE TABLE Info 
-	(id AUTO_INCREMENT PRIMARY KEY,
+	(id int AUTO_INCREMENT PRIMARY KEY,
 	numer_telefonu VARCHAR(11) NOT NULL,
 	HASH VARCHAR(64) NOT NULL,
 	TIMESTAMP TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	kraj VARCHAR(50) NOT NULL);
-
--- dodawanie rekordów (id i timestamp dodają się automatycznie, więc ich nie wpisujemy): 	
-
-INSERT INTO Info (numer_telefonu, HASH, kraj)
-VALUES ()
+	kraj VARCHAR(50));
